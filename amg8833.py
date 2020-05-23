6515efe8c7e6365fcb26e48d05ef4bcdff0cbc83
KettleRunner <crousetech@gmail.com>
2020-05-22 22:58:28 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -24,7 +24,7 @@ points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0,64)]
 grid_x, grid_y = np.mgrid[0:7:512j, 0:7:512j]
 
 ax2 = fig.add_subplot(222)
-hist = ax2.hist(amg.pixels, color = 'blue', edgecolor = 'black', bins = int(180/5))
+hist = ax2.hist(amg.pixels, bins = int(180/5))
 
 while True:
     ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax((9/5)*np.amax(amg.pixels)+32)))
