07d316df169b61a04dd545d5b9deb87840021d07
KettleRunner <crousetech@gmail.com>
2020-05-22 23:02:58 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -14,8 +14,9 @@ plt.ion()
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 fig = plt.figure(num='AMG8833 Thermal Scanner');
+fig = plt.figure(figsize=(8,8))
 
-ax = fig.add_subplot(111)
+ax = fig.add_subplot(222)
 ax.set_yticklabels([])
 ax.set_xticklabels([])
 im = ax.imshow(amg.pixels, cmap='jet', vmin=vmin, vmax=vmax)
