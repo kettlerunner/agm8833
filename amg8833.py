91d3fdc9dc03b59101e8e6d095b13104940c0674
KettleRunner <crousetech@gmail.com>
2020-05-23 19:26:11 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -17,29 +17,29 @@ body_temp_array = []
 body_temp = 98.6
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
-fig = plt.figure(num='AMG8833 Thermal Scanner');
+#fig = plt.figure(num='AMG8833 Thermal Scanner');
 
-ax = fig.add_subplot(121)
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-im = ax.imshow(amg.pixels, cmap='jet', vmin=vmin, vmax=vmax)
-fig.colorbar(im, ax=ax)
+#ax = fig.add_subplot(121)
+#ax.set_yticklabels([])
+#ax.set_xticklabels([])
+#im = ax.imshow(amg.pixels, cmap='jet', vmin=vmin, vmax=vmax)
+#fig.colorbar(im, ax=ax)
 points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0,64)]
 grid_x, grid_y = np.mgrid[0:7:256j, 0:7:256j]
 
-ax2 = fig.add_subplot(122)
+#ax2 = fig.add_subplot(122)
 
 while True:
-    ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax((9/5)*np.amax(amg.pixels)+32)))
+    #ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax((9/5)*np.amax(amg.pixels)+32)))
     pixels = np.fliplr(np.rot90(np.asarray(amg.pixels), k=3)).flatten()
     pixels_f = (9/5)*pixels+32
-    ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax(pixels_f )))
+    #ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax(pixels_f )))
     grid_0 = griddata(points, pixels_f, (grid_x, grid_y), method='cubic')
-    im.set_data(grid_0)
-    ax2.clear()
+    #im.set_data(grid_0)
+    #ax2.clear()
     flat_grid = grid_0.flatten()
     hist, bin_edges = np.histogram(flat_grid, bins=256)
-    bar = ax2.bar(bin_edges[:-1], hist, width = 0.1, color='#0504aa',alpha=0.7)
+    #bar = ax2.bar(bin_edges[:-1], hist, width = 0.1, color='#0504aa',alpha=0.7)
     human_flat_grid = flat_grid[flat_grid > 70.0]
     human_flat_grid = human_flat_grid[human_flat_grid < 85.0]
     room_flat_grid = flat_grid[flat_grid > 50.0]
@@ -67,4 +67,4 @@ while True:
     else:
         collecting_body_temps = False
         body_temp_array = []
-    fig.canvas.draw()
+    #fig.canvas.draw()
