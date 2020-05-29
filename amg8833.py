8936b14520c832828e2a33e411003e197af86f70
KettleRunner <crousetech@gmail.com>
2020-05-29 16:59:40 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -14,7 +14,7 @@ vmin = 63
 plt.ion()
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
-fig = plt.figure(num='AMG8833 Thermal Scanner', figsize=(2.0, 2.0);
+fig = plt.figure(num='AMG8833 Thermal Scanner', figsize=(2.0, 2.0));
 points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0,64)]
 grid_x, grid_y = np.mgrid[0:7:256j, 0:7:256j]
 
