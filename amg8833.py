d78854286be4a1624184b6fb12c73d440c322d27
KettleRunner <crousetech@gmail.com>
2020-05-29 15:53:10 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -7,7 +7,6 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
-from scipy.signal import find_peaks
 
 vmax = 80
 vmin = 63
@@ -15,6 +14,7 @@ plt.ion()
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 fig = plt.figure(num='AMG8833 Thermal Scanner');
+fig = plt.figure()
 
 ax = fig.add_subplot(121)
 ax.set_yticklabels([])
@@ -32,26 +32,8 @@ while True:
     pixels_f = (9/5)*pixels+32
     ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax(pixels_f )))
     grid_0 = griddata(points, pixels_f, (grid_x, grid_y), method='cubic')
-    im.set_data(pixels_f)
+    im.set_data(grid_0)
     ax2.clear()
-    flat_grid = grid_0.flatten()
-    hist, bin_edges = np.histogram(flat_grid, bins=256)
-    bar = ax2.bar(bin_edges[:-1], hist, width = 0.1, color='#0504aa',alpha=0.7)
-    human_flat_grid = flat_grid[flat_grid > 70.0]
-    human_flat_grid = human_flat_grid[human_flat_grid < 85.0]
-    room_flat_grid = flat_grid[flat_grid > 50.0]
-    room_flat_grid = room_flat_grid[room_flat_grid < 72.0]
-    if len(room_flat_grid) > 0:
-        hist, bin_edges = np.histogram(room_flat_grid, bins=256)
-        bin_width = bin_edges[0] - bin_edges[1]
-        peaks, _ = find_peaks(hist, height=150)
-        room_temp = np.amax(bin_edges[peaks]) + bin_width / 2
-        print("Room Temp: {0:.4f}".format(room_temp))
-        
-    if len(human_flat_grid) > 0:
-        hist, bin_edges = np.histogram(flat_grid, bins=256)
-        bin_width = bin_edges[0] - bin_edges[1]
-        peaks, _ = find_peaks(hist, height=400)
-        human_temp = np.amax(bin_edges[peaks]) + bin_width / 2
-        print("Human Temp: {0:.4f}".format(human_temp))
+    hist = ax2.hist(grid_0, bins = 2)
+    #hist.set_data(grid_0)
     fig.canvas.draw()
