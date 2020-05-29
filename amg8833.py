544dab320cc8f0608945f07331b75bbd07d8f42e
KettleRunner <crousetech@gmail.com>
2020-05-29 16:51:29 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -24,7 +24,7 @@ while True:
     pixels = np.fliplr(np.rot90(np.asarray(amg.pixels), k=3)).flatten()
     pixels_f = (9/5)*pixels+32
     grid_0 = griddata(points, pixels_f, (grid_x, grid_y), method='cubic')
-    ax2.clear()
+    ax.clear()
     surf = ax2.plot_surface(grid_x, grid_y, grid_0, cmap="jet", linewidth=0, antialiased=False)
     ax.view_init(75, angle)
     fig.canvas.draw()
