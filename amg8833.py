960f25057ab9e55d16ff52fa8da95b5988c8326b
KettleRunner <crousetech@gmail.com>
2020-05-29 18:38:07 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -18,20 +18,16 @@ amg = adafruit_amg88xx.AMG88XX(i2c)
 fig = plt.figure(num='AMG8833 Thermal Scanner', figsize=(4.0, 4.0));
 points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0,64)]
 grid_x, grid_y = np.mgrid[0:7:512j, 0:7:512j]
-
 ax = fig.add_subplot(111, projection='3d')
-import seaborn as sns
-ax.axes.get_xaxis().set_visible(False)
-ax.axes.get_yaxis().set_visible(False)
-
 angle = 0
 while True:
     pixels = np.fliplr(np.rot90(np.asarray(amg.pixels), k=3)).flatten()
     pixels_f = (9/5)*pixels+32
     grid_0 = griddata(points, pixels_f, (grid_x, grid_y), method='cubic')
-    
     ax.clear()
     surf = ax.plot_surface(grid_x, grid_y, grid_0, cmap="jet", linewidth=0, antialiased=False)
+    surf.axes.get_xaxis().set_visible(False)
+    surf.axes.get_yaxis().set_visible(False)
     #surf = ax.plot_trisurf(grid_x, grid_y, grid_0, cmap="jet", linewidth=0, antialiased=False)
     ax.view_init(80, angle)
     fig.tight_layout()
