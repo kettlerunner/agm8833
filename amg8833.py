7975643957878920e44f998c42290b67822a61ef
KettleRunner <crousetech@gmail.com>
2020-05-29 16:18:28 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -23,7 +23,7 @@ fig.colorbar(im, ax=ax)
 points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0,64)]
 grid_x, grid_y = np.mgrid[0:7:256j, 0:7:256j]
 
-ax2 = fig.add_subplot(122)
+ax2 = fig.add_subplot(122, projection='3d')
 
 while True:
     ax.set_title("Max Temp Found: {0:.1f}F".format(np.amax((9/5)*np.amax(amg.pixels)+32)))
@@ -33,23 +33,10 @@ while True:
     grid_0 = griddata(points, pixels_f, (grid_x, grid_y), method='cubic')
     im.set_data(grid_0)
     ax2.clear()
-    hist = ax2.hist(grid_0, bins = 2)
+    surf = ax2.plot_surface(grid_x, grid_y, grid_0, rstride=6, cstride=6, cmap=cm.jet,
+            linewidth=0)
+    #hist = ax2.hist(grid_0, bins = 2)
     #hist.set_data(grid_0)
     fig.canvas.draw()
-    
-    ax = fig.add_subplot(111, projection='3d')
-    xi = np.linspace(0, 256)
-    yi = np.linspace(0, 256)
-    X, Y = np.meshgrid(xi, yi)
-    Z = griddata(x, y, z, xi, yi)
-    surf = ax.plot_surface(X, Y, Z, rstride=6, cstride=6, cmap=cm.jet,
-            linewidth=0)
-
-ax.set_zlim3d(min(z), max(z))
-
-ax.w_zaxis.set_major_locator(LinearLocator(10))
-ax.w_zaxis.set_major_formatter(FormatStrFormatter('%.03f'))
-
-fig.colorbar(surf, shrink=0.5, aspect=5)
 
 plt.show()
