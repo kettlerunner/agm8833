49f6b4f44a61b8b15cdee24168eb78741083f21e
KettleRunner <crousetech@gmail.com>
2020-05-29 21:20:38 -0500

    Update amg8833.py

--- amg8833.py
+++ amg8833.py
@@ -15,11 +15,14 @@ vmin = 63
 plt.ion()
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
-fig = plt.figure(num='AMG8833 Thermal Scanner', figsize=(4.0, 4.0));
+fig = plt.figure(num='AMG8833 Thermal Scanner', figsize=(8.0, 4.0));
 points = [(math.floor(ix / 8), (ix % 8)) for ix in range(0,64)]
 grid_x, grid_y = np.mgrid[0:7:512j, 0:7:512j]
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.add_subplot(121, projection='3d')
 ax.set_axis_off()
+angle = 0
+ax1 = fig.add_subplot(121, projection='3d')
+ax1.set_axis_off()
 angle = 0
 while True:
     pixels = np.fliplr(np.rot90(np.asarray(amg.pixels), k=3)).flatten()
@@ -29,6 +32,15 @@ while True:
     surf = ax.plot_surface(grid_x, grid_y, grid_z, cmap="jet", linewidth=0, antialiased=False)
     ax.view_init(80, angle)
     ax.set_axis_off()
+    
+    flat_grid = grid_z.flatten()
+    hist, bin_edges = np.histogram(flat_grid, bins=16)
+    grid_z[grid_z < bin_edges[len(bin_edges) - 1]] = 0
+    ax1.clear()
+    surf1 = ax1.plot_surface(grid_x, grid_y, grid_z, cmap="jet", linewidth=0, antialiased=False)
+    ax1.view_init(80, angle)
+    ax1.set_axis_off()
+    
     fig.tight_layout()
     fig.canvas.draw()
 
